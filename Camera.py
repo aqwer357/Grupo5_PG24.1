3b7d42f5387cc6d940775197f45f88c7c5fd9791
Maike Menezes <maikermenezes@gmail.com>
2024-05-17 17:10:16 -0300

    Adding method to calculate the distance between two points and screen dimensions

--- Camera.py
+++ Camera.py
@@ -1,3 +1,4 @@
+import math
 from Elements import Point, Vector
 
 def pointSubtract(p1: Point, p2: Point):
@@ -14,18 +15,43 @@ def cross(v1: Vector, v2: Vector):
                      v1.x*v2.y - v2.y*v2.x,)
     return product
 
+
+def pointDistance(p1: Point, p2: Point):
+
+    difference = math.pow((p1.x - p2.x), 2) + math.pow((p1.y - p2.y), 2) + math.pow((p1.z - p2.z), 2)
+
+    distance = math.sqrt(difference)
+
+    return round(distance, 2)
+
 class Camera:
-    def __init__(self, pos: Point, target: Point, up: Vector):
+    def __init__(self, pos: Point, target: Point, up: Vector, screen_width: int, screen_height: int):
         self.pos = pos
         self.target = target
     
         # Vectors of R3
         self.w = pointSubtract(pos, target)
         self.u = cross(self.w, up)
+        self.v = cross(self.w, self.u)
         self.up = up
+
+        # Screen Dimensions
+
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        # Distance from camera to screen
+
+        self.distance = pointDistance(pos, target)
+
+
+    def rayCast():
+        return 0
+    
 
     def __repr__(self):  # __repr__ is used for debugging
         return f"Camera({self.pos}, {self.target}, {self.up})"
 
     def __str__(self):  # __str__ is used for printing
         return f"({self.pos}, {self.target}, {self.up})"
+    
